0d2a64d76115d3138f3ea5c1849887db350538de
Can Bascil <canbascil@google.com>
2015-05-03 22:01:58 +0100

    Fix project name

--- convex-hull-fp/project.clj
+++ convex-hull-fp/project.clj
@@ -1,4 +1,4 @@
-(defproject pascals-triangle "0.1.0"
+(defproject convex-hull-fp "0.1.0"
   :description "Convex Hull FP"
   :url "https://www.hackerrank.com/challenges/convex-hull-fp"
   :dependencies [[org.clojure/clojure "1.6.0"]]
